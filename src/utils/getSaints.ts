2347a1fe21a7900299a36c18537479f0beed5a65
Mourão <p3dr0sk0sk7160@gmail.com>
2026-03-04 03:30:38 -0300

    atualizado getSaints.ts

--- src/utils/getSaints.ts
+++ src/utils/getSaints.ts
@@ -1,7 +1,7 @@
 import { Saint } from '../types/Saint.js';
 
 export async function getSaints(): Promise<Saint[]> {
-    let resposta: Response = await fetch('../data.json');
+    let resposta: Response = await fetch('../../docs/data.json');
     let dados: Saint[] = await resposta.json();
     return dados;
 };
